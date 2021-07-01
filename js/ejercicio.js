ba0c6344233069c88a331b8221d07e382be0827f
DiegaG <diego.garciasanchez@gmail.com>
2021-06-30 22:35:54 -0300

    clase 3

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,29 +1,23 @@
-/*Constantes*/
-const generoF = "Femenino";
-const generoM = "Masculino";
-const generoO = "Otro";
-const edadJubHombre = 65;
-const edadJubMujer = 60;
-
 /*Variables Globales*/
-let nombre = prompt("Ingrese su nombre");
-let edad = parseInt(prompt("Ingrese su edad"));
-let genero = prompt("Ingrese su Genero: Femenino | Masculino | Otro  ");
+// ejercicio 1
+/* let suma = 0;
+let ingreso= parseInt(prompt("Ingrese un numero para sumarlo , para finalizar ingrese una letra. "));
 
+while(ingreso != 'ESC' && parseInt(ingreso)){
+    suma += ingreso
+    console.log("La suma de los numeros ingresados es: " + suma)
+    ingreso = parseInt(prompt("Ingrese un numero para sumarlo , para finalizar ingrese una letra. "))
+}
 
-if(genero === generoF || genero === generoM || genero === generoO  ){
-    if ((genero == generoF && edad >= edadJubMujer ) || ((genero == generoM || genero == generoO) && edad >= edadJubHombre )){
-        alert("Felicitaciones ud deberia ser Jubilado")
-    }else if(genero == generoF){
-            //Variable del ifs
-            var jubilacion = edadJubMujer - edad
-            alert(nombre + " le restan " + jubilacion + " años para jubilarse")
-        }else{
-            jubilacion = edadJubHombre - edad
-            alert(nombre + " le restan " + jubilacion + " años para jubilarse")
-        }
-    if (jubilacion >= 0 && jubilacion <= 5 ){
-        console.log("Fuerza, te restan pocos años mas")
-    } else{ console.log("Animos, a seguir aportando!!!")}
-    
-}else{alert("Genero incorrecto")} 
+console.log("terminamos")
+ */
+//ejercicio 2
+let frase = ""
+let ingreso = prompt("ingrese una palabra")
+
+while(ingreso.toUpperCase() !== "ESC"){
+    frase += ingreso
+    console.log("La frase por ahora es: " + suma)
+    ingreso = parseInt(prompt("Ingrese otra palabra para sumar a la frase , para finalizar ingrese 'ESC' "))
+    }
+console.log("su frase es: '" + frase +"'");
