2971f6957c70159b1796c53e1032d3841a437418
DiegaG <diego.garciasanchez@gmail.com>
2021-07-05 20:44:24 -0300

    clase 3

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -12,7 +12,7 @@ while(ingreso != 'ESC' && parseInt(ingreso)){
 console.log("terminamos")
  */
 //ejercicio 2
-let frase = ""
+/* let frase = ""
 let ingreso = prompt("ingrese una palabra")
 
 while(ingreso.toUpperCase() !== "ESC"){
@@ -21,3 +21,43 @@ while(ingreso.toUpperCase() !== "ESC"){
     ingreso = parseInt(prompt("Ingrese otra palabra para sumar a la frase , para finalizar ingrese 'ESC' "))
     }
 console.log("su frase es: '" + frase +"'");
+ */
+
+
+//Calculadora
+
+let opcion = prompt("Seleccione una operacion: 1-Sumar | 2-Restar | 3-Multiplicar | 4-Dividir , para finalizar ingrese '5' ");
+                              
+while(opcion !== '5' ){
+//los tuve que inicializar aca, por qe dentro del switch no podia
+    let num1;
+    let num2;
+    switch (opcion){
+
+        case "1":
+            num1 = parseInt(prompt("ingrese el primer numero a Sumar "))
+            num2 = parseInt(prompt("ingrese el segundo numero a Sumar "))
+            alert("La suma de "+ num1 + " mas " + num2 + " es de: " + (num1+num2)) 
+            break
+        case "2":
+            num1 = parseInt(prompt("ingrese el primer numero a Restar "))
+            num2 = parseInt(prompt("ingrese el segundo numero a Restar "))
+            alert("La resta de "+ num1 + " menos " + num2 + " es de: " + (num1-num2))
+            break    
+        case "3":
+            num1 = parseInt(prompt("ingrese el primer numero a Multiplicar "))
+            num2 = parseInt(prompt("ingrese el segundo numero a Multiplicar "))
+            alert("La multiplicacion de "+ num1 + " por " + num2 + " es de: " + (num1*num2))
+            break
+        case "4":
+            num1 = parseFloat(prompt("ingrese el primer numero a dividir "))
+            num2 = parseFloat(prompt("ingrese el segundo numero a dividir "))
+            alert("La division de "+ num1 + " dividido " + num2 + " es de: " + (num1/num2))
+            break 
+        default:    
+            alert("Ha ingresado una opcion invalida")
+            break
+    }
+    opcion = prompt("Seleccione una operacion: 1-Sumar | 2-Restar | 3-Multiplicar | 4-Dividir , para finalizar ingrese 'ESC' ")
+
+} 
