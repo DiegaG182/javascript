63b23106bca02639c6be89715f5068895cf11a8b
DiegaG <diego.garciasanchez@gmail.com>
2021-07-14 22:47:57 -0300

    clase 4 funciones + adicional+ clase 5 class

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,3 +1,29 @@
+//Calculadora coutas - Clase 4
+//[3,6,12] cuotas
+
+/* const interesCuota = [221,210,200];
+const iva = 21;
+const calcularImpuestos = (precio,impuesto) => { return (precio += precio * impuesto /100);}
+
+let precio = parseFloat(prompt("Ingrese el costo neto del producto"));
+if (precio > 0) {
+imprimirIva(precio);
+listarCuotas(precio)
+} else{alert("El importe debe ser mayor a cero")}
+
+function imprimirIva(precio) {
+    alert("el precio del producto con iva es de : " + calcularImpuestos(precio,iva))
+}
+
+function listarCuotas(precio){
+    for(i=3, c=0; c<interesCuota.length ;i*=2, c++){
+        alert("el precio final del producto en " + i + " cuotas es de $ " + calcularImpuestos(calcularImpuestos(precio,iva),interesCuota[c]) +  " ,el precio de cada cuota es de $ " + (calcularImpuestos(calcularImpuestos(precio,iva),interesCuota[c])/i))
+    }
+}
+ */
+
+//Creacion de persona para app de entrega de perros
+// clase 5
 //Objetos
 
 class Persona{
@@ -31,32 +57,6 @@ class Persona{
     
 }
 
-//Calculadora coutas - Clase 4
-//[3,6,12] cuotas
-
-/* const interesCuota = [221,210,200];
-const iva = 21;
-const calcularImpuestos = (precio,impuesto) => { return (precio += precio * impuesto /100);}
-
-let precio = parseFloat(prompt("Ingrese el costo neto del producto"));
-if (precio > 0) {
-imprimirIva(precio);
-listarCuotas(precio)
-} else{alert("El importe debe ser mayor a cero")}
-
-function imprimirIva(precio) {
-    alert("el precio del producto con iva es de : " + calcularImpuestos(precio,iva))
-}
-
-function listarCuotas(precio){
-    for(i=3, c=0; c<interesCuota.length ;i*=2, c++){
-        alert("el precio final del producto en " + i + " cuotas es de $ " + calcularImpuestos(calcularImpuestos(precio,iva),interesCuota[c]) +  " ,el precio de cada cuota es de $ " + (calcularImpuestos(calcularImpuestos(precio,iva),interesCuota[c])/i))
-    }
-}
- */
-
-//Creacion de persona para app de entrega de perros
-// clase 5
 agregarPersona = () => {
     nombre = prompt("ingrese su nombre");
     edad = parseInt(prompt("ingrese su edad"));
